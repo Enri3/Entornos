7695cf84c9a46d27a0b3d879f1f81122f08b51eb
Agus <abarrosobol@frro.utn.edu.ar>
2024-11-14 17:38:47 -0300

    Corregido un error

--- practica7/ejercicio2/index.php
+++ practica7/ejercicio2/index.php
@@ -1,10 +1,30 @@
+<!-- 
+Crear una cookie llamada “contador” que lleve la cuenta en el lado cliente del número de veces que
+se ha accedido a la página contador.php. Si es la primera vez que se accede, la página dará la
+bienvenida al usuario. Si ya se ha accedido anteriormente, la página hará uso de la cookie para
+mostrar el número de veces que se ha visitado dicha página.
+-->
+
+<?php
+
+        if (!isset($_COOKIE['visitas'])) {
+            $visitas = 1;
+            setcookie("visitas", $visitas , time()+ 3600*24*365);
+        }
+        else {
+            $visitas = $_COOKIE['visitas'] + 1;
+            setcookie("visitas", $visitas , time()+ 3600*24*365);
+        }
+?>
+
+
 <!DOCTYPE html>
 
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Práctica 5 Ejercicio 2</title>
+        <title>Práctica 7 Ejercicio 2</title>
     </head>
     <body>
 
