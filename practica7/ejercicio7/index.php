3d34ab21bb600025a7e206a69761d1433a1b8d04
Agus <abarrosobol@frro.utn.edu.ar>
2024-11-14 18:26:21 -0300

    Sinceramente no me acuerdo que cambié

--- practica7/ejercicio7/index.php
+++ practica7/ejercicio7/index.php
@@ -1,3 +1,12 @@
+<!-- 
+Confeccionar un CARRITO DE COMPRAS simple, usando Base de Datos. Se debe crear una base de
+datos con el nombre Compras. En dicha base crear una tabla llamada catálogo con los siguientes
+atributos:
+id
+producto del tipo varchar de 100
+precio del tipo numérico decimal de 9 entero y 2 decimales
+-->
+
 <?php
 // Iniciar la sesión
 session_start();
