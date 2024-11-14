f819a237003947c69f9bf5a4733a204649c06d5a
Agus <abarrosobol@frro.utn.edu.ar>
2024-11-14 17:38:24 -0300

    Ejercicio 3

--- practica7/ejercicio3/index.php
+++ practica7/ejercicio3/index.php
@@ -1,20 +1,19 @@
 <!-- 
-Crear una página que puede configurarse con distintos estilos CSS. El usuario es quien decide qué
-aspecto desea que tenga la página, por medio de un formulario. Luego la página es capaz de
-recordar, entre los distintos accesos que realice el usuario, el aspecto que había elegido para
-mostrar la web. 
+Crear un formulario que solicite la carga del nombre de usuario. Cuando se presione un botón
+crear una cookie para dicho usuario. Luego cada vez que ingrese al formulario mostrar el último
+nombre de usuario ingresado. 
 -->
 
 <?php
 
 //Ver su recibo datos y creo la cookie, si no recibí nada chequeo la cookie
 
-if(isset($_POST["estilo"])){
-    $estilo = $_POST["estilo"];
-    setcookie("estilo", $estilo, time() + (60*60*24*90));
+if(isset($_POST["nombre"])){
+    $nombre = $_POST["nombre"];
+    setcookie("nombre", $nombre, time() + (60*60*24*90));
 }else{
-    if (isset($_COOKIE["estilo"])){
-        $estilo = $_COOKIE["estilo"];
+    if (isset($_COOKIE["nombre"])){
+        $nombre = $_COOKIE["nombre"];
     }
 }
 ?>
@@ -25,26 +24,26 @@ if(isset($_POST["estilo"])){
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <?php
-
-        if(isset($estilo)){
-            echo '<link rel="stylesheet" type="text/css" href="'.$estilo.'.css">';
-        };
-        ?>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>  
         <title>Práctica 5 Ejercicio 3</title>
     </head>
     <body>
 
-    <form action="" method="post">
-        Selección de estilos de la página
-        <select name="estilo" id="estilo">
-            <option value="verde">Verde</option>
-            <option value="rosa">Rosa</option>
-            <option value="negro">Negro</option>
-        </select>
-        <input type="submit" value="Actualizar">
-    </form>
+        <?php
+    
+            if(isset($nombre)){
+                echo '<p>Bienvenido! '.$nombre.' </p>';
+            };
+        ?>
+        <form action="" method="post">
+        
+                <div class="mb-3">
+                    <label for="nombre" class="form-label">Nombre</label>
+                    <input type="text" class="form-control" id="nombre" placeholder="Nombre" name="nombre">
+                </div>
+            <button type="submit" class="btn btn-primary" name="enviar">Enviar</button>
+        </form>
 
     </body>
 </html>
